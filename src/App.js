14f383ebe6015afa13f805a67e0823d5535d5256
Phai <phai.339@gmail.com>
2025-01-28 23:05:46 +0700

    update

--- src/App.js
+++ src/App.js
@@ -16,12 +16,12 @@ const App = () => {
     <div id='top' className={`${themeName} app`}>
       <Header />
 
-      <div>
+      <main>
         <About />
         <Projects />
         <Skills />
         <Contact />
-      </div>
+      </main>
 
       <ScrollToTop />
       <Footer />
